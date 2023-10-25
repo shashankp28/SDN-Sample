6d0fe2a8b6eec87fc63b69fd0d817ea2cb790baf
Shashank P <shashankp2832@gmail.com>
2023-10-25 15:26:11 +0530

    Updated RING topology

--- Mininet/ring.py
+++ Mininet/ring.py
@@ -29,7 +29,7 @@ class MyTopology(Topo):
 topo = MyTopology(6)
 net = Mininet(
     topo=topo,
-    controller=lambda name: RemoteController(name, ip='172.17.0.2', port=6633),
+    controller=lambda name: RemoteController(name, ip='172.17.0.3', port=6633),
     switch=OVSSwitch,
     autoSetMacs=True
 )
